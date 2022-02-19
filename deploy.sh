4689762c7be2434d79e17e087d8477e074a48237
PL4typus <pierre-louis.palant@insa-cvl.fr>
2022-02-19 14:13:12 +0100

    Fixing deploy action

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,22 @@
 set -e
-echo "Publishing to ${GITHUB_REPOSITORY} on branch gh-pages"
-cp -rv _site .. && \
-ls -la && \
+cd _site
+echo "::debug::Initializing new repo"
+REMOTE_REPO="https://${GITHUB_ACTOR}:${INPUT_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
+LOCAL_BRANCH="main"
+git init -b $LOCAL_BRANCH
+PUSH_OPTIONS="--force"
+COMMIT_OPTIONS=""
+remote_branch="gh-pages"
+
+echo "Publishing to ${GITHUB_REPOSITORY} on branch ${remote_branch}"
+
 git config user.name "${GITHUB_ACTOR}" && \
 git config user.email "${GITHUB_ACTOR}@users.noreply.github.com" && \
-git checkout gh-pages && \
-FILES=$(ls) && \
-rm -rfv $FILES && \
-cp -rv ../_site/* . && \
 git add . && \
-git commit -m "jekyll build from Action ${GITHUB_SHA}" && \
-git push -u origin gh-pages \
-git add . && \
+git commit $COMMIT_OPTIONS -m "jekyll build from Action ${GITHUB_SHA}" && \
+git push $PUSH_OPTIONS $REMOTE_REPO $LOCAL_BRANCH:$remote_branch && \
+echo "::set-output name=SHA::$( git rev-parse ${LOCAL_BRANCH} )"
 rm -fr .git && \
+cd .. 
 
 exit $?
